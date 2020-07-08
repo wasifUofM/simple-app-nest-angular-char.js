351d6dbc79885152e19391964bdd22a66b823a8f
wasif ahmed <wasifahmed@MacBook-Pro.local>
2020-07-07 22:13:18 -0400

    add swagger documentation

--- fake-umbrella-api/src/main.ts
+++ fake-umbrella-api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Fake Umbrella API')
+    .setDescription('Weather forecast using OpenWeather Map')
+    .setVersion('4.5.12')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(3000);
 }
