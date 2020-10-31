824e96d7e932c10b62e6e123cc60a33d99456951
wasif ahmed <wasifahmed@MacBook-Pro.local>
2020-10-30 21:19:45 -0400

    rename folder

--- fake-umbrella-api/src/config/keys.ts
+++ fake-umbrella-api/src/config/keys.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoURI: 'mongodb+srv://<Password>@cluster0-ohkp4.mongodb.net/customers?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://admin:Admin123!@cluster0-ohkp4.mongodb.net/customers?w=majority&retryWrites=true,',
     top_k_customer : 4
 
 }
